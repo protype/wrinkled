2adb44192201f5988f92169dfd95a47eac87d28a
Wake Liu <wake.gs@gmail.com>
2019-10-01 23:17:53 +0800

    Add router and handler for adding url API

--- app/api/url.php
+++ app/api/url.php
@@ -3,6 +3,7 @@
 
   use Illuminate\Http\Request;
   use Illuminate\Validation\ValidationException;
+  use App\Helper\Shorty;
 
 
   /**
@@ -44,34 +45,60 @@
    * Create url
    *
    */
-  $router->post ('/api/url', ['as' => 'api/url/add', function (Request $request) use ($app) {
+  $router->post ('/@/api/url', ['as' => 'api/url/add', function (Request $request) use ($app) {
 
     $data = $request->json ()->all ();
+    $code = '';
+    $error = [];
 
-    /*
-    $count = \Model::Factory ('Model\Url')
-      ->count ('id');
+    try {
+      $this->validate ($request,
+        Model\Url::$_rules,
+        Model\Url::$_messages);
+    }
 
-    $limit = $app->request->query->get ('limit', 20);
-    $offset = $app->request->query->get ('offset', 0);
+    catch (ValidationException $e) {
+      $errors = current ($e->errors ());
+      $error = [
+        'status' => 400,
+        'title' => $errors[0]
+      ];
+    }
 
-    $limit = $limit < 1 ? 1 : ($limit > 100 ? 100 : $limit);
-    $offset = $offset < 0 ? 0 : ($offset > $count ? $count : $offset);
+    if (empty ($error) && isset ($data['code']) && $data['code'] != '') {
 
-    $urls = \Model::Factory ('Model\Url')
-      ->order_by_desc ('id')
-      ->limit ($limit)
-      ->offset ($offset)
-      ->find_array ();
+      $url = Model::Factory ('Model\Url')
+        ->where ('code', $data['code'])
+        ->find_one ();
+
+      if ($url)
+        $error = [
+          'status' => 409,
+          'title' => "The shorten code `{$data['code']}` has already existed."
+        ];
+
+      $code = $data['code'];
+    }
+
+    if (! empty ($error))
+      return response ()->json (['errors' => [$error]], $error['status'], [], JSON_UNESCAPED_UNICODE);
+
+    $url = Model::Factory ('Model\Url')->create ();
+    $url->code = $code;
+    $url->url = $data['url'];
+    $url->title = array_get ($data, 'title', null);
+    $url->description = array_get ($data, 'description', null);
+    $url->save ();
+
+    if ($url->code == '') {
+      $url->code = Shorty::encode ($url->id);
+      $url->save ();
+    }
 
     $resp = [
-      'meta' => [
-        'totalItems' => $count
-      ],
-      'data' => $urls,
+      'data' => $url->as_array (),
     ];
-    */
 
-    return response ()->json ($data, 200, [], JSON_UNESCAPED_UNICODE);
+    return response ()->json ($resp, 200, [], JSON_UNESCAPED_UNICODE);
 
   }]);
