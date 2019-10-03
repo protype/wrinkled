d8b89f2879600a7860438a9f15229c5690498591
Wake Liu <wake.gs@gmail.com>
2019-10-04 02:45:42 +0800

    To improve handing performance, avoid using framework

--- public/index.php
+++ public/index.php
@@ -3,15 +3,59 @@
 
   /**
    *
-   * Create the application
+   * Root
    *
    */
-  $app = require __DIR__ . '/../bootstrap/app.php';
+  define ('_ROOT', dirname (__DIR__));
 
 
   /**
    *
-   * Run the application
+   * Vendors
    *
    */
-  $app->run ();
+  require_once _ROOT . '/vendor/autoload.php';
+
+
+  /**
+   *
+   * Environments
+   *
+   */
+  try {
+    Dotenv\Dotenv::create (_ROOT)->load ();
+  }
+
+  catch (Dotenv\Exception\InvalidPathException $e) {
+  }
+
+
+  /**
+   *
+   * Initial DB connection & model
+   *
+   */
+  require_once _ROOT . '/config/database.php';
+  require_once _ROOT . '/database/models/url.php';
+
+
+  /**
+   *
+   * Prepare
+   *
+   */
+  $host = array_get ($_SERVER, 'HTTP_HOST', null);
+  $uri = array_get ($_SERVER, 'REQUEST_URI', null);
+
+  if (is_null ($host) || is_null ($uri))
+    header ("HTTP/1.0 404 Not Found") && exit;
+
+  $url = Model::Factory ('Model\Url')
+    ->where ('short_url', "{$host}{$uri}")
+    ->find_one ();
+
+  if (! $url)
+    header ("HTTP/1.0 404 Not Found") && exit;
+
+  header ("Location: {$url->original_url}", true, 302);
+  exit;
