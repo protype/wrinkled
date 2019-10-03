f996dd13215707e10c4b5dd13cb610490006eb12
Wake Liu <wake.gs@gmail.com>
2019-10-04 03:15:37 +0800

    Fixes exit not work issue

--- public/index.php
+++ public/index.php
@@ -47,15 +47,19 @@
   $host = array_get ($_SERVER, 'HTTP_HOST', null);
   $uri = array_get ($_SERVER, 'REQUEST_URI', null);
 
-  if (is_null ($host) || is_null ($uri))
-    header ("HTTP/1.0 404 Not Found") && exit;
+  if (is_null ($host) || is_null ($uri)) {
+    header ("HTTP/1.0 404 Not Found");
+    exit;
+  }
 
   $url = Model::Factory ('Model\Url')
     ->where ('short_url', "{$host}{$uri}")
     ->find_one ();
 
-  if (! $url)
-    header ("HTTP/1.0 404 Not Found") && exit;
+  if (! $url) {
+    header ("HTTP/1.0 404 Not Found");
+    exit;
+  }
 
   header ("Location: {$url->original_url}", true, 302);
   exit;
