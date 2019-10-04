f6cc16458db9fd6386211a0e84e80a5a4a24934e
Wake Liu <wake.gs@gmail.com>
2019-10-05 02:41:23 +0800

    Re-design by using namecheap logo maker

--- resource/vue/src/app.vue
+++ resource/vue/src/app.vue
@@ -6,15 +6,17 @@
 <template>
   <div id="app">
     <header>
-      <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="navbar navbar-dark bg-wrinkled shadow-sm">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
+            <img src="@/assets/img/logo.svg" width="20" height="20" class="mr-2" />
             <strong>Wrinkled</strong>
           </a>
+          <!--
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+          -->
         </div>
       </div>
     </header>
@@ -39,7 +41,7 @@
                 -->
                 <input type="text" class="form-control splice-input" :class="{'is-invalid': action.long.invalid}" @keyup.enter="shorten" placeholder="Paste long URL" v-model="action.long.value">
                 <div class="input-group-append">
-                  <button type="button" @click="shorten" class="btn-lg btn-secondary ml-3">Shorten</button>
+                  <button type="button" @click="shorten" class="btn-lg btn-wrinkled ml-3">Shorten</button>
                 </div>
               </div>
 
@@ -114,7 +116,7 @@
 
               <hr class="mb-4" />
 
-              <button class="btn btn-secondary btn-lg btn-block mt-4" @click="update" :disabled="action.id == 0">Save</button>
+              <button class="btn btn-wrinkled btn-lg btn-block mt-4" @click="update" :disabled="action.id == 0">Save</button>
 
             </div>
           </div>
@@ -192,7 +194,7 @@
     </main>
 
     <footer class="my-4 pt-4 text-muted text-center text-small">
-      <p class="mb-1">&copy; 2019 Wrinkled</p>
+      <p class="mb-1">&copy; 2019 Protype</p>
     </footer>
 
   </div>
